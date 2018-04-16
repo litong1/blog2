e42777423d07ada2a1c3c4bd8494de881c5ad6d8
lt <lt@guangdongjizhi.com>
2018-04-16 17:53:56 +0800

    留言板和文章统计

--- blog2/src/main/java/com/lt/blog/mapper/ArticleCountMapper.java
+++ blog2/src/main/java/com/lt/blog/mapper/ArticleCountMapper.java
@@ -1,6 +1,46 @@
 package com.lt.blog.mapper;
 
+import java.util.List;
+
+import com.lt.blog.pojo.ArticleCount;
+
 public interface ArticleCountMapper {
 
-	
+	/**
+	 * 增加文章统计记录
+	 * @param articleCount
+	 */
+	public void addArticleCount(ArticleCount articleCount);
+	/**
+	 * 通过id得到文章统计记录
+	 * @param articleid
+	 * @return
+	 */
+	public ArticleCount getArticleCountByArticleId(Integer articleid);
+	/**
+	 * 通过用户id得到文章统计记录
+	 * @param userid
+	 * @return
+	 */
+	public List<ArticleCount> getArticleCountByUserId(Integer userid);
+	/**
+	 * 增加文章浏览量
+	 * @param articleCount
+	 */
+	public void addArticleViewCount(ArticleCount articleCount); 
+	/**
+	 * 增加文章收藏数
+	 * @param articleCount
+	 */
+	public void addArticleCollectedCount(ArticleCount articleCount);
+	/**
+	 * 增加文章评论数
+	 * @param articleCount
+	 */
+	public void addArticleCommentedCount(ArticleCount articleCount);
+	/**
+	 * 增加文章喜欢数
+	 * @param articleCount
+	 */
+	public void addArticleLikedCount(ArticleCount articleCount);
 }
